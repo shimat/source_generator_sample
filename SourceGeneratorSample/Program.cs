d6005f547aca73ea186f23f573657b904122f67a
shimat <schimatk@gmail.com>
2022-11-28 04:13:41 +0900

    refactor

--- SourceGeneratorSample/Program.cs
+++ SourceGeneratorSample/Program.cs
@@ -1,6 +1,4 @@
-﻿using SourceGeneratorSample.Generators;
-
-namespace SourceGeneratorSample;
+﻿namespace SourceGeneratorSample;
 
 class Program
 {
@@ -11,47 +9,5 @@ class Program
             //new RunnerStjCustom();
             //new RunnerJsonNet();
         runner.Run();
-
-        /*
-        var foo = new Foo(
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9,
-            0,1,2,3,4,5,6,7,8,9);
-        //*/
-
-        //var json = $"{{ {string.Join(", ", Enumerable.Range(0, 64).Select(i => $"\"V{i}\": {i}"))} }}";
-        //var json = $"{{ {string.Join(", ", Enumerable.Range(0, 2480).Select(i => $"\"V{i}\": {i}"))} }}";
-        //var json = $"{{ {string.Join(", ", Enumerable.Range(0, 502).Select(i => $"\"V{i}\": {i}"))} }}";
-        //var foo = JsonSerializer.Deserialize<Foo>(json);
-        //var foo = Newtonsoft.Json.JsonConvert.DeserializeObject<Foo>(json);
-        /*var foo = JsonSerializer.Deserialize<Foo>(json, new JsonSerializerOptions
-        {
-            Converters = { new FooConverterFactory() }
-        });*/
-        //Console.WriteLine(foo);
-
-        /*
-        var json = $"{{ {string.Join(", ", Enumerable.Range(0, 65).Select(i => $"\"V{i}\": {i}"))} }}";
-        var bar = JsonSerializer.Deserialize<Bar>(json);
-        Console.WriteLine(bar);
-        */
     }
 }
